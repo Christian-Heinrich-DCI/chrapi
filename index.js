7870274a81905726b8e8cc30c0545f6817e2db9f
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2024-09-25 12:37:04 +0200

    init

---
+++ index.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import fs from 'fs';
+
+dotenv.config();
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Ihr braucht noch /<Org>');
+});
+
+app.get('/atlassian', (req, res) => {
+  fs.readFile('./json/atlassian.json', (err, json) => {
+    let obj = JSON.parse(json);
+    res.json(obj);
+  });
+});
+
+app.get('/canonical', (req, res) => {
+  fs.readFile('./json/canonical.json', (err, json) => {
+    let obj = JSON.parse(json);
+    res.json(obj);
+  });
+});
+
+app.get('/microsoft', (req, res) => {
+  fs.readFile('./json/microsoft.json', (err, json) => {
+    let obj = JSON.parse(json);
+    res.json(obj);
+  });
+});
+
+app.get('/oracle', (req, res) => {
+  fs.readFile('./json/oracle.json', (err, json) => {
+    let obj = JSON.parse(json);
+    res.json(obj);
+  });
+});
+
+app.get('/robert-koch-institut', (req, res) => {
+  fs.readFile('./json/robert-koch-institut.json', (err, json) => {
+    let obj = JSON.parse(json);
+    res.json(obj);
+  });
+});
+
+app.listen(process.env.PORT || 3456, () => {
+  console.log('Backend running on port: ' + (process.env.PORT || 3456));
+});
